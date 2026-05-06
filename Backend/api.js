95d7b05863b3a5cf92e30d0c9e5e6a39829ef778
yiaraalsaied <yiaraalsaied06@gmail.com>
2026-05-06 17:56:18 +0200

    Update api.js

--- Backend/api.js
+++ Backend/api.js
@@ -33,50 +33,59 @@ function getGenres(request) {
 
 async function createMovieReview(request) {
     if (request.headers.get("Content-Type") == "application/json") {
-        const body = await request.json();
-        const db = readData();
+        try {
+            const body = await request.json();
+            const db = readData();
 
-        let maxId = 0;
+            let maxId = 0;
 
-        for (let movie of db.movies) {
-            const id = parseInt(movie.id);
-            if (id > maxId) maxId = id;
-        }
-
-        let newId = maxId + 1;
-
-        let genreId = null;
-        for (let genre of db.genre) {
-            if (genre.name === body.genre) {
-                genreId = genre.id;
-                break;
+            for (let movie of db.movies) {
+                const id = parseInt(movie.id);
+                if (id > maxId) maxId = id;
             }
-        }
 
-        const newMovie = {
-            id: newId,
-            title: body.title,
-            year: body.year,
-            genreId: genreId,
-            genre: body.genre,
-            director: body.director,
-            runtime: body.runtime,
-            posterUrl: body.posterUrl,
-            description: body.description,
-            status: body.status
-        };
+            let newId = maxId + 1;
 
-        db.movies.push(newMovie);
-
-        writeData(db);
-
-        return new Response(null, {
-            status: 201,
-            headers: {
-                "Access-Control-Allow-Origin": "*"
+            let genreId = null;
+            for (let genre of db.genre) {
+                if (genre.name === body.genre) {
+                    genreId = genre.id;
+                    break;
+                }
             }
-        });
 
+            const newMovie = {
+                id: newId,
+                title: body.title,
+                year: body.year,
+                genreId: genreId,
+                genre: body.genre,
+                director: body.director,
+                runtime: body.runtime,
+                posterUrl: body.posterUrl,
+                description: body.description,
+                status: body.status
+            };
+
+            db.movies.push(newMovie);
+
+            writeData(db);
+
+            return new Response(null, {
+                status: 201,
+                headers: {
+                    "Access-Control-Allow-Origin": "*"
+                }
+            });
+        } catch (error) {
+            return new Response(JSON.stringify({}), {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            });
+        }
     } else {
         return new Response("Invalid Content Type", {
             status: 406,
