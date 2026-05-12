d1d88c202ec2c8bf1ea6c8e83f27c0c5cb85a4d0
janettajane <khatuevamariam9@gmail.com>
2026-05-12 22:37:26 +0200

    Update api.js

--- Backend/api.js
+++ Backend/api.js
@@ -578,7 +578,7 @@ async function postSignUp(request) {
             id: newId,
             email: body.email,
             passwordHash: body.password,
-            token: "Bearer" + " " + crypto.ramdomUUID(),
+            token: crypto.ramdomUUID(),
         }
 
         data.users.push(newUser);
