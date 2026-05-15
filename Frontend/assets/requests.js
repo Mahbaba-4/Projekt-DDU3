1180a034d7a741ab2b8541844edabeb201c0bb53
janettajane <khatuevamariam9@gmail.com>
2026-05-15 13:22:23 +0200

    Update requests.js

--- Frontend/assets/requests.js
+++ Frontend/assets/requests.js
@@ -20,6 +20,23 @@ class API {
             console.log(error.message);
         }
     }
+
+    async logIn(email, password){
+        try {
+
+        const response = await fetch ();
+            
+        if(response.ok){
+            const data = await response.json();
+            return data;
+        } else {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+    } catch (error){
+        console.log(error.message);
+    }
+    }
 }
 
 //http://localhost:8000/
