39b010ee1639ab30c9d5fa833378142d2cfedb0c
yiaraalsaied <yiaraalsaied06@gmail.com>
2026-05-15 13:30:43 +0200

    Update requests.js

--- Frontend/assets/requests.js
+++ Frontend/assets/requests.js
@@ -2,7 +2,7 @@ class API {
     //sign --> ett objekt som tar emot ett objet med 3 nycklar som är {username, email, password};
     async postSignUp(sign) {
         try {
-            const response = await fetch ("http://localhost:8000/auth/signup", {
+            const response = await fetch("http://localhost:8000/auth/signup", {
                 method: "POST",
                 hedares: {
                     "Content-Type": "application/json"
@@ -10,7 +10,7 @@ class API {
                 body: JSON.stringify(sign)
             });
 
-            if(response.ok) {
+            if (response.ok) {
                 window.location.href = "/login.html";
             } else {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -21,27 +21,27 @@ class API {
         }
     }
 
-    async logIn(email, password){
+    async logIn(email, password) {
         try {
 
-        const response = await fetch ("http://localhost:8000/auth/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({email, password})
-        });
-            
-        if(response.ok){
-            const data = await response.json();
-            return data;
-        } else {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            const response = await fetch("http://localhost:8000/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+        } catch (error) {
+            console.log(error.message);
         }
-    
-    } catch (error){
-        console.log(error.message);
-    }
     }
 }
 
