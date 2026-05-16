5852278e21c8d627350b982451a4ada331f6656a
janettajane <khatuevamariam9@gmail.com>
2026-05-17 01:09:11 +0200

    Update movie.js

--- Frontend/assets/movie.js
+++ Frontend/assets/movie.js
@@ -3,4 +3,14 @@ class UI {
         this.api = apiInstance;
         this.currentProductId = null;
     }
+
+    signUpForm(){
+        const signUpForm = document.getElementById("movieForm");
+
+        signUpForm.addEventListener("submit", async function(e) {e.preventDefault()
+
+            const usernameInput = signUpForm.querySelector(``)
+        })
+    }
 }
+
