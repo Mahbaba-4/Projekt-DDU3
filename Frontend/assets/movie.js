aca6dd9825e86d9ed85446fb9fc0b3d6056224af
janettajane <khatuevamariam9@gmail.com>
2026-05-17 01:24:53 +0200

    LogIn and SignUp created in movie.js

--- Frontend/assets/movie.js
+++ Frontend/assets/movie.js
@@ -13,13 +13,35 @@ class UI {
             const emailInput = signUpForm.getElementById("email").value;
             const passwordInput = signUpForm.getElementById("password").value;
 
-            if(!username || !email || !password){
+            if(!usernameInput || !emailInput || !passwordInput){
                 this.showError("Alla fält måste fyllas in");
                 return;
             }
 
-            await this.api.postSignUp({username, email, password})
+            await this.api.postSignUp({
+                username: usernameInput,
+                email: emailInput, 
+                password: passwordInput
+            })
         })
+    }
+
+    logInForm(){
+        const logInForm = document.getElementById("movieForm");
+
+        logInForm.addEventListener("submit", async function(e) {e.preventDefault()
+
+            const usernameInput = document.getElementById("username").value;
+            const passwordInput = document.getElementById("password").value;
+
+            if(!username || !password){
+                this.showError("Användarnamn och lösenord krävs för att logga in");
+                return;
+            }
+
+            await this.api.logIn(usernameInput, passwordInput)
+        })
+
     }
 }
 
