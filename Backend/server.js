3eaec57eb6ca208fbfd96191dd246d543201ec7b
Mahbaba-4 <ureenbabur@gmail.com>
2026-05-17 19:38:52 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,24 +6,6 @@ const movieByIdRoute = new URLPattern({ pathname: "/user/movies/:id" });
 const genreByIdRoute = new URLPattern({ pathname: "/movie/genre/:id" })
 const listByIdRoute = new URLPattern({ pathname: "/user/lists/:id" })
 
-function corsHeaders(request) {
-    const origin = request.headers.get("Origin");
-    if (origin) {
-        return {
-            "Access-Control-Allow-Origin": origin,
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Authorization, Content-Type, Accept",
-            "Access-Control-Max-Age": "86400"
-        };
-    }
-    return {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Authorization, Content-Type, Accept"
-    };
-}
-
 
 function handler(request) {
     let url = new URL(request.url);
@@ -31,7 +13,12 @@ function handler(request) {
     if (request.method === "OPTIONS") {
         return new Response(null, {
             status: 204,
-            headers: corsHeaders(request)
+             headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Authorization, Content-Type, Accept",
+                "Access-Control-Max-Age": "86400"
+            }
         });
     }
 
