668e89c83d51860c265cbe0fd132ce82146607a5
Mahbaba-4 <ureenbabur@gmail.com>
2026-05-17 23:20:06 +0200

    update postprofileImage

--- Backend/api.js
+++ Backend/api.js
@@ -967,14 +967,7 @@ async function postProfileImage(request) {
         const newFilename = uniqueName + extension;
 
         const bytes = await imageFile.bytes();
-
-        try {
-            await Deno.mkdir("./uploads/profile-images", { recursive: true });
-        } catch (error) {
-            console.log("Error");
-        }
-
-
+            
         const filePath = `./uploads/profile-images/${newFilename}`;
         await Deno.writeFile(filePath, bytes);
 
@@ -1007,71 +1000,6 @@ async function postProfileImage(request) {
             }
         });
     } catch (error) {
-        return new Response(JSON.stringify({}), {
-            status: 500,
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            }
-        });
-    }
-}
-
-async function deleteProfileImage(request) {
-    try {
-
-        const userId = getUserIdFromSession(request);
-        if (!userId) {
-            return new Response(JSON.stringify({ error: "Not logged in" }), {
-                status: 401,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                }
-            });
-        }
-
-        const data = readData();
-        let user = null;
-        for (let i = 0; i < data.users.length; i++) {
-            if (data.users[i].id === userId) {
-                user = data.users[i];
-                break;
-            }
-        }
-
-        if (!user) {
-            return new Response(JSON.stringify({ error: "User not found" }), {
-                status: 404,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                }
-            });
-        }
-
-        if (user.profileImage && user.profileImage.substring(0, 9) === "/uploads/") {
-            const filePath = `.${user.profileImage}`;
-            try {
-                await Deno.remove(filePath);
-            } catch (error) {
-                console.log("Could not remove image:", error);
-            }
-        }
-
-        user.profileImage = null;
-        writeData(data);
-
-        return new Response(null, {
-            status: 204,
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            }
-        });
-
-
-
-    } catch (error) {
-        console.error(error);
         return new Response(JSON.stringify({}), {
             status: 500,
             headers: {
