2b08f248e34bc96e661aec89a3d3c3fa2cbc3add
janettajane <khatuevamariam9@gmail.com>
2026-05-17 15:47:40 +0200

    Update requests.js

--- Frontend/assets/requests.js
+++ Frontend/assets/requests.js
@@ -11,7 +11,7 @@ class API {
             });
 
             if (response.ok) {
-                window.location.href = "/login.html";
+                window.location.href = "login.html";
             } else {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
