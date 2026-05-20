544841065231420eb3e3b6f4a56ea004e1906758
Mahbaba-4 <ureenbabur@gmail.com>
2026-05-20 12:08:59 +0200

    .

--- Backend/api.js
+++ Backend/api.js
@@ -665,7 +665,7 @@ async function postLogIn(request) {
             headers: {
                 "Content-Type": "application/json",
                 "Set-Cookie": "sessionId=" + sessionId + "; Max-Age=86400; Path=/; SameSite=Lax;"
-                //Path vilka path cookies ska skickas till, SameSite=None; Secure; krävs för att cookies ska skickas i cross-site requests 
+                //Path vilka path cookies ska skickas till, SameSite=Lax; Secure; krävs för att cookies ska skickas i cross-site requests (för login, bilder åch sånt ska skickas)
             }
         })
 
